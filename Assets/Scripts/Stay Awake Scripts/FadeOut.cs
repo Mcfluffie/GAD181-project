490d1a0dbe76bb6e7e0dccfc0ad650274f98aa4d
Matt Dawson <matthewdawson3512@gmail.com>
2021-10-28 09:28:41 +0800

    Fade out for Stay Awake

--- Assets/Scripts/Stay Awake Scripts/FadeOut.cs
+++ Assets/Scripts/Stay Awake Scripts/FadeOut.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeOut : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject blackOutSquare;
+
+    public void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(FadeBlackOutSquare());
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(FadeBlackOutSquare(false));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public IEnumerator FadeBlackOutSquare(bool fadetoblack = true, int fadeSpeed = 5)
     {
-        
+        Color objectColor = blackOutSquare.GetComponent<Image>().color;
+        float fadeAmount;
+
+        if (fadetoblack)
+        {
+            while (blackOutSquare.GetComponent<Image>().color.a < 1)
+            {
+                fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+
+            }
+        } else
+        {
+            while (blackOutSquare.GetComponent<Image>().color.a > 0)
+            {
+                fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+                objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+                blackOutSquare.GetComponent<Image>().color = objectColor;
+                yield return null;
+            }
+        }
+
     }
+
 }
